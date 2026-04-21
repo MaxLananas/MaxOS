7b44d79a15b955d9053004b2ba9c29ac5aaa6a11
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-21 18:55:59 +0000

    Update kernel_entry.asm

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,8 +1,17 @@
+BITS 32
 global _start
+global _stack_top
 extern kmain
+
+section .bss
+resb 16384
+_stack_top:
 
 section .text
 _start:
     mov esp, _stack_top
     call kmain
-    jmp $
+.hang:
+    cli
+    hlt
+    jmp .hang
