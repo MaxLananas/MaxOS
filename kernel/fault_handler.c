d5d90dae01ac4b46cc0f69cfccdf0b994ef7e387
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 05:00:35 +0000

    Update fault_handler.c

--- kernel/fault_handler.c
+++ kernel/fault_handler.c
@@ -1,14 +1,25 @@
-#include "fault_handler.h"
-#include "screen.h"
-#include "idt.h"
+#include "kernel/fault_handler.h"
+#include "drivers/screen.h"
+
+static void itoa_hex(unsigned int n, char *buf) {
+    int i;
+    buf[0] = '0'; buf[1] = 'x';
+    for (i = 9; i >= 2; i--) {
+        unsigned int d = n & 0xF;
+        buf[i] = (d < 10) ? ('0' + d) : ('A' + d - 10);
+        n >>= 4;
+    }
+    buf[10] = 0;
+}
 
 void fault_handler(unsigned int num, unsigned int err) {
-    screen_set_color(0x0C);
-    screen_writeln("Exception: ", 0x0C);
-    screen_putchar('0' + num / 10, 0x0C);
-    screen_putchar('0' + num % 10, 0x0C);
-    screen_writeln("", 0x0C);
-    while (1) {
-        asm volatile("hlt");
-    }
+    char buf[11];
+    screen_write("FAULT #", 0x0C);
+    itoa_hex(num, buf);
+    screen_write(buf, 0x0C);
+    screen_write(" ERR=", 0x0C);
+    itoa_hex(err, buf);
+    screen_write(buf, 0x0C);
+    screen_write("\n", 0x07);
+    __asm__ volatile("cli; hlt");
 }
