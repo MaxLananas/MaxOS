b63aa37d2f446c7eea4d9d7cad6479604e498ed8
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:48:51 +0000

    Update idt.h

--- kernel/idt.h
+++ kernel/idt.h
@@ -1,7 +1,38 @@
 #ifndef IDT_H
 #define IDT_H
 
+struct IDTEntry {
+    unsigned short base_lo;
+    unsigned short sel;
+    unsigned char  always0;
+    unsigned char  flags;
+    unsigned short base_hi;
+} __attribute__((packed));
+
+struct IDTPtr {
+    unsigned short limit;
+    unsigned int   base;
+} __attribute__((packed));
+
 void idt_init(void);
 void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags);
+
+extern void isr0(void);  extern void isr1(void);  extern void isr2(void);
+extern void isr3(void);  extern void isr4(void);  extern void isr5(void);
+extern void isr6(void);  extern void isr7(void);  extern void isr8(void);
+extern void isr9(void);  extern void isr10(void); extern void isr11(void);
+extern void isr12(void); extern void isr13(void); extern void isr14(void);
+extern void isr15(void); extern void isr16(void); extern void isr17(void);
+extern void isr18(void); extern void isr19(void); extern void isr20(void);
+extern void isr21(void); extern void isr22(void); extern void isr23(void);
+extern void isr24(void); extern void isr25(void); extern void isr26(void);
+extern void isr27(void); extern void isr28(void); extern void isr29(void);
+extern void isr30(void); extern void isr31(void);
+extern void isr32(void); extern void isr33(void); extern void isr34(void);
+extern void isr35(void); extern void isr36(void); extern void isr37(void);
+extern void isr38(void); extern void isr39(void); extern void isr40(void);
+extern void isr41(void); extern void isr42(void); extern void isr43(void);
+extern void isr44(void); extern void isr45(void); extern void isr46(void);
+extern void isr47(void);
 
 #endif
