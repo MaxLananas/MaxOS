d33ea2253010a0b2adcc3dc2b266a7f12ff35a21
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:59:59 +0000

    Update memory.h

--- kernel/memory.h
+++ kernel/memory.h
@@ -1,10 +1,9 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-void mem_init(unsigned int start, unsigned int end);
+void mem_init(unsigned int mem_size_kb);
+void *mem_alloc_page(void);
 void mem_free_page(void *addr);
 unsigned int mem_used_pages(void);
-void heap_init(void *start, unsigned int size);
-void heap_free(void *ptr);
 
 #endif
