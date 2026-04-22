62ad45d2448114f37f2412c7ad690079f14756d3
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:58:15 +0000

    Update idt.c

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,69 +1,84 @@
-#ifndef IDT_H
-#define IDT_H
+#include "kernel/idt.h"
+#include "kernel/io.h"
 
-struct IDTEntry {
-    unsigned short base_lo;
-    unsigned short sel;
-    unsigned char  always0;
-    unsigned char  flags;
-    unsigned short base_hi;
-} __attribute__((packed));
+static struct IDTEntry idt[256];
+static struct IDTPtr   idt_ptr;
 
-struct IDTPtr {
-    unsigned short limit;
-    unsigned int   base;
-} __attribute__((packed));
+static void idt_load(void) {
+    __asm__ volatile("lidt (%0)" :: "r"(&idt_ptr));
+}
 
-void idt_init(void);
-void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags);
+void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags) {
+    idt[num].base_lo  = (base & 0xFFFF);
+    idt[num].base_hi  = (base >> 16) & 0xFFFF;
+    idt[num].sel      = sel;
+    idt[num].always0  = 0;
+    idt[num].flags    = flags;
+}
 
-extern void isr0(void);
-extern void isr1(void);
-extern void isr2(void);
-extern void isr3(void);
-extern void isr4(void);
-extern void isr5(void);
-extern void isr6(void);
-extern void isr7(void);
-extern void isr8(void);
-extern void isr9(void);
-extern void isr10(void);
-extern void isr11(void);
-extern void isr12(void);
-extern void isr13(void);
-extern void isr14(void);
-extern void isr15(void);
-extern void isr16(void);
-extern void isr17(void);
-extern void isr18(void);
-extern void isr19(void);
-extern void isr20(void);
-extern void isr21(void);
-extern void isr22(void);
-extern void isr23(void);
-extern void isr24(void);
-extern void isr25(void);
-extern void isr26(void);
-extern void isr27(void);
-extern void isr28(void);
-extern void isr29(void);
-extern void isr30(void);
-extern void isr31(void);
-extern void isr32(void);
-extern void isr33(void);
-extern void isr34(void);
-extern void isr35(void);
-extern void isr36(void);
-extern void isr37(void);
-extern void isr38(void);
-extern void isr39(void);
-extern void isr40(void);
-extern void isr41(void);
-extern void isr42(void);
-extern void isr43(void);
-extern void isr44(void);
-extern void isr45(void);
-extern void isr46(void);
-extern void isr47(void);
+void idt_init(void) {
+    idt_ptr.limit = (sizeof(struct IDTEntry) * 256) - 1;
+    idt_ptr.base  = (unsigned int)&idt;
 
-#endif
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0xFF);
+    outb(0xA1, 0xFF);
+
+    idt_set_gate(0,  (unsigned int)isr0,  0x08, 0x8E);
+    idt_set_gate(1,  (unsigned int)isr1,  0x08, 0x8E);
+    idt_set_gate(2,  (unsigned int)isr2,  0x08, 0x8E);
+    idt_set_gate(3,  (unsigned int)isr3,  0x08, 0x8E);
+    idt_set_gate(4,  (unsigned int)isr4,  0x08, 0x8E);
+    idt_set_gate(5,  (unsigned int)isr5,  0x08, 0x8E);
+    idt_set_gate(6,  (unsigned int)isr6,  0x08, 0x8E);
+    idt_set_gate(7,  (unsigned int)isr7,  0x08, 0x8E);
+    idt_set_gate(8,  (unsigned int)isr8,  0x08, 0x8E);
+    idt_set_gate(9,  (unsigned int)isr9,  0x08, 0x8E);
+    idt_set_gate(10, (unsigned int)isr10, 0x08, 0x8E);
+    idt_set_gate(11, (unsigned int)isr11, 0x08, 0x8E);
+    idt_set_gate(12, (unsigned int)isr12, 0x08, 0x8E);
+    idt_set_gate(13, (unsigned int)isr13, 0x08, 0x8E);
+    idt_set_gate(14, (unsigned int)isr14, 0x08, 0x8E);
+    idt_set_gate(15, (unsigned int)isr15, 0x08, 0x8E);
+    idt_set_gate(16, (unsigned int)isr16, 0x08, 0x8E);
+    idt_set_gate(17, (unsigned int)isr17, 0x08, 0x8E);
+    idt_set_gate(18, (unsigned int)isr18, 0x08, 0x8E);
+    idt_set_gate(19, (unsigned int)isr19, 0x08, 0x8E);
+    idt_set_gate(20, (unsigned int)isr20, 0x08, 0x8E);
+    idt_set_gate(21, (unsigned int)isr21, 0x08, 0x8E);
+    idt_set_gate(22, (unsigned int)isr22, 0x08, 0x8E);
+    idt_set_gate(23, (unsigned int)isr23, 0x08, 0x8E);
+    idt_set_gate(24, (unsigned int)isr24, 0x08, 0x8E);
+    idt_set_gate(25, (unsigned int)isr25, 0x08, 0x8E);
+    idt_set_gate(26, (unsigned int)isr26, 0x08, 0x8E);
+    idt_set_gate(27, (unsigned int)isr27, 0x08, 0x8E);
+    idt_set_gate(28, (unsigned int)isr28, 0x08, 0x8E);
+    idt_set_gate(29, (unsigned int)isr29, 0x08, 0x8E);
+    idt_set_gate(30, (unsigned int)isr30, 0x08, 0x8E);
+    idt_set_gate(31, (unsigned int)isr31, 0x08, 0x8E);
+    idt_set_gate(32, (unsigned int)isr32, 0x08, 0x8E);
+    idt_set_gate(33, (unsigned int)isr33, 0x08, 0x8E);
+    idt_set_gate(34, (unsigned int)isr34, 0x08, 0x8E);
+    idt_set_gate(35, (unsigned int)isr35, 0x08, 0x8E);
+    idt_set_gate(36, (unsigned int)isr36, 0x08, 0x8E);
+    idt_set_gate(37, (unsigned int)isr37, 0x08, 0x8E);
+    idt_set_gate(38, (unsigned int)isr38, 0x08, 0x8E);
+    idt_set_gate(39, (unsigned int)isr39, 0x08, 0x8E);
+    idt_set_gate(40, (unsigned int)isr40, 0x08, 0x8E);
+    idt_set_gate(41, (unsigned int)isr41, 0x08, 0x8E);
+    idt_set_gate(42, (unsigned int)isr42, 0x08, 0x8E);
+    idt_set_gate(43, (unsigned int)isr43, 0x08, 0x8E);
+    idt_set_gate(44, (unsigned int)isr44, 0x08, 0x8E);
+    idt_set_gate(45, (unsigned int)isr45, 0x08, 0x8E);
+    idt_set_gate(46, (unsigned int)isr46, 0x08, 0x8E);
+    idt_set_gate(47, (unsigned int)isr47, 0x08, 0x8E);
+
+    idt_load();
+}
