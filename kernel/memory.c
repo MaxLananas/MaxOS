c98a3c303c874ad46aa63e276175f4d7f9c5cfab
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 05:00:10 +0000

    Update memory.c

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,19 +1,54 @@
-#include "memory.h"
-#include "screen.h"
+#include "kernel/memory.h"
 
-void mem_init(unsigned int start, unsigned int end) {
-    screen_writeln("Memory initialized", 0x0A);
+#define MAX_PAGES 32768
+#define PAGE_SIZE 4096
+#define KERNEL_END 0x100000
+
+static unsigned int bitmap[MAX_PAGES / 32];
+static unsigned int total_pages = 0;
+static unsigned int used_pages  = 0;
+
+static void bitmap_set(unsigned int page) {
+    bitmap[page / 32] |= (1 << (page % 32));
 }
 
-void mem_free_page(void *addr) {
+static void bitmap_clear(unsigned int page) {
+    bitmap[page / 32] &= ~(1 << (page % 32));
 }
 
-unsigned int mem_used_pages(void) {
+static int bitmap_test(unsigned int page) {
+    return bitmap[page / 32] & (1 << (page % 32));
+}
+
+void mem_init(unsigned int mem_size_kb) {
+    unsigned int i;
+    total_pages = (mem_size_kb * 1024) / PAGE_SIZE;
+    if (total_pages > MAX_PAGES) total_pages = MAX_PAGES;
+    for (i = 0; i < MAX_PAGES / 32; i++) bitmap[i] = 0;
+    for (i = 0; i < KERNEL_END / PAGE_SIZE; i++) bitmap_set(i);
+    used_pages = KERNEL_END / PAGE_SIZE;
+}
+
+void *mem_alloc_page(void) {
+    unsigned int i;
+    for (i = 0; i < total_pages; i++) {
+        if (!bitmap_test(i)) {
+            bitmap_set(i);
+            used_pages++;
+            return (void *)(i * PAGE_SIZE);
+        }
+    }
     return 0;
 }
 
-void heap_init(void *start, unsigned int size) {
+void mem_free_page(void *addr) {
+    unsigned int page = (unsigned int)addr / PAGE_SIZE;
+    if (page < total_pages && bitmap_test(page)) {
+        bitmap_clear(page);
+        used_pages--;
+    }
 }
 
-void heap_free(void *ptr) {
+unsigned int mem_used_pages(void) {
+    return used_pages;
 }
