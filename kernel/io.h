ff3a7331037cd464628ff42ea3e7ca7cdc04b31e
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:57:44 +0000

    Update io.h

--- kernel/io.h
+++ kernel/io.h
@@ -1,7 +1,28 @@
 #ifndef IO_H
 #define IO_H
 
-unsigned char inb(unsigned short port);
-void outb(unsigned short port, unsigned char data);
+static inline void outb(unsigned short port, unsigned char val) {
+    __asm__ volatile("outb %0, %1" :: "a"(val), "Nd"(port));
+}
+
+static inline unsigned char inb(unsigned short port) {
+    unsigned char val;
+    __asm__ volatile("inb %1, %0" : "=a"(val) : "Nd"(port));
+    return val;
+}
+
+static inline void outw(unsigned short port, unsigned short val) {
+    __asm__ volatile("outw %0, %1" :: "a"(val), "Nd"(port));
+}
+
+static inline unsigned short inw(unsigned short port) {
+    unsigned short val;
+    __asm__ volatile("inw %1, %0" : "=a"(val) : "Nd"(port));
+    return val;
+}
+
+static inline void io_wait(void) {
+    outb(0x80, 0x00);
+}
 
 #endif
