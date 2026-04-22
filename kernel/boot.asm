82c4eeaf8acf07803703045d04f02a5490acb713
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:53:37 +0000

    Update boot.asm

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -1,32 +1,68 @@
-[org 0x7C00]
-[bits 16]
+BITS 16
+ORG 0x7C00
 
 start:
+    cli
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
+    sti
+    mov [boot_drive], dl
+    mov ah, 0x02
+    mov al, 32
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, [boot_drive]
+    mov bx, 0x1000
+    mov es, bx
+    mov bx, 0x0000
+    int 0x13
+    jc .disk_error
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp 0x08:protected_mode
 
-    mov si, msg
-    call print_string
-
-    mov ah, 0x00
-    mov al, 0x03
-    int 0x10
-
-    jmp 0x10000
-
-print_string:
+.disk_error:
+    mov si, msg_err
+.loop:
     lodsb
     or al, al
-    jz .done
+    jz .halt
     mov ah, 0x0E
     int 0x10
-    jmp print_string
-.done:
-    ret
+    jmp .loop
+.halt:
+    hlt
 
-msg db "Booting...", 0
+BITS 32
+protected_mode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+    jmp 0x10000
+
+gdt_start:
+    dq 0x0000000000000000
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+boot_drive: db 0
+msg_err:    db 'Boot error', 0
+
 times 510-($-$$) db 0
 dw 0xAA55
