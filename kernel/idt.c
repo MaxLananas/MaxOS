85902810e9fc46b284e483ebc20dc237b534aaf2
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:58:02 +0000

    Update idt.c

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,41 +1,69 @@
-#include "idt.h"
-#include "io.h"
-
-#define IDT_SIZE 256
+#ifndef IDT_H
+#define IDT_H
 
 struct IDTEntry {
-    unsigned short base_low;
+    unsigned short base_lo;
     unsigned short sel;
-    unsigned char always0;
-    unsigned char flags;
-    unsigned short base_high;
+    unsigned char  always0;
+    unsigned char  flags;
+    unsigned short base_hi;
 } __attribute__((packed));
 
-struct IDTPointer {
+struct IDTPtr {
     unsigned short limit;
-    unsigned int base;
+    unsigned int   base;
 } __attribute__((packed));
 
-struct IDTEntry idt[IDT_SIZE];
-struct IDTPointer idtp;
+void idt_init(void);
+void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags);
 
-extern void idt_load();
+extern void isr0(void);
+extern void isr1(void);
+extern void isr2(void);
+extern void isr3(void);
+extern void isr4(void);
+extern void isr5(void);
+extern void isr6(void);
+extern void isr7(void);
+extern void isr8(void);
+extern void isr9(void);
+extern void isr10(void);
+extern void isr11(void);
+extern void isr12(void);
+extern void isr13(void);
+extern void isr14(void);
+extern void isr15(void);
+extern void isr16(void);
+extern void isr17(void);
+extern void isr18(void);
+extern void isr19(void);
+extern void isr20(void);
+extern void isr21(void);
+extern void isr22(void);
+extern void isr23(void);
+extern void isr24(void);
+extern void isr25(void);
+extern void isr26(void);
+extern void isr27(void);
+extern void isr28(void);
+extern void isr29(void);
+extern void isr30(void);
+extern void isr31(void);
+extern void isr32(void);
+extern void isr33(void);
+extern void isr34(void);
+extern void isr35(void);
+extern void isr36(void);
+extern void isr37(void);
+extern void isr38(void);
+extern void isr39(void);
+extern void isr40(void);
+extern void isr41(void);
+extern void isr42(void);
+extern void isr43(void);
+extern void isr44(void);
+extern void isr45(void);
+extern void isr46(void);
+extern void isr47(void);
 
-void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags) {
-    idt[num].base_low = base & 0xFFFF;
-    idt[num].base_high = (base >> 16) & 0xFFFF;
-    idt[num].sel = sel;
-    idt[num].always0 = 0;
-    idt[num].flags = flags;
-}
-
-void idt_init(void) {
-    idtp.limit = (sizeof(struct IDTEntry) * IDT_SIZE) - 1;
-    idtp.base = (unsigned int)&idt;
-
-    for (int i = 0; i < IDT_SIZE; i++) {
-        idt_set_gate(i, 0, 0, 0);
-    }
-
-    idt_load();
-}
+#endif
