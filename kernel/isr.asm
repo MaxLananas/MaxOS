afadc31e3db74d41d390dd933fd6f66d6575c963
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-22 04:57:14 +0000

    Update isr.asm

--- kernel/isr.asm
+++ kernel/isr.asm
@@ -52,55 +52,198 @@ global isr47
 extern isr_handler
 extern irq_handler
 
-isr0:  push 0  \n  push 0  \n  jmp isr_common_stub
-isr1:  push 0  \n  push 1  \n  jmp isr_common_stub
-isr2:  push 0  \n  push 2  \n  jmp isr_common_stub
-isr3:  push 0  \n  push 3  \n  jmp isr_common_stub
-isr4:  push 0  \n  push 4  \n  jmp isr_common_stub
-isr5:  push 0  \n  push 5  \n  jmp isr_common_stub
-isr6:  push 0  \n  push 6  \n  jmp isr_common_stub
-isr7:  push 0  \n  push 7  \n  jmp isr_common_stub
-isr8:  push 8  \n  jmp isr_common_stub
-isr9:  push 0  \n  push 9  \n  jmp isr_common_stub
-isr10: push 10 \n  jmp isr_common_stub
-isr11: push 11 \n  jmp isr_common_stub
-isr12: push 12 \n  jmp isr_common_stub
-isr13: push 13 \n  jmp isr_common_stub
-isr14: push 14 \n  jmp isr_common_stub
-isr15: push 0  \n  push 15 \n  jmp isr_common_stub
-isr16: push 0  \n  push 16 \n  jmp isr_common_stub
-isr17: push 17 \n  jmp isr_common_stub
-isr18: push 0  \n  push 18 \n  jmp isr_common_stub
-isr19: push 0  \n  push 19 \n  jmp isr_common_stub
-isr20: push 0  \n  push 20 \n  jmp isr_common_stub
-isr21: push 0  \n  push 21 \n  jmp isr_common_stub
-isr22: push 0  \n  push 22 \n  jmp isr_common_stub
-isr23: push 0  \n  push 23 \n  jmp isr_common_stub
-isr24: push 0  \n  push 24 \n  jmp isr_common_stub
-isr25: push 0  \n  push 25 \n  jmp isr_common_stub
-isr26: push 0  \n  push 26 \n  jmp isr_common_stub
-isr27: push 0  \n  push 27 \n  jmp isr_common_stub
-isr28: push 0  \n  push 28 \n  jmp isr_common_stub
-isr29: push 0  \n  push 29 \n  jmp isr_common_stub
-isr30: push 0  \n  push 30 \n  jmp isr_common_stub
-isr31: push 0  \n  push 31 \n  jmp isr_common_stub
-
-isr32: push 0  \n  push 32 \n  jmp irq_common_stub
-isr33: push 0  \n  push 33 \n  jmp irq_common_stub
-isr34: push 0  \n  push 34 \n  jmp irq_common_stub
-isr35: push 0  \n  push 35 \n  jmp irq_common_stub
-isr36: push 0  \n  push 36 \n  jmp irq_common_stub
-isr37: push 0  \n  push 37 \n  jmp irq_common_stub
-isr38: push 0  \n  push 38 \n  jmp irq_common_stub
-isr39: push 0  \n  push 39 \n  jmp irq_common_stub
-isr40: push 0  \n  push 40 \n  jmp irq_common_stub
-isr41: push 0  \n  push 41 \n  jmp irq_common_stub
-isr42: push 0  \n  push 42 \n  jmp irq_common_stub
-isr43: push 0  \n  push 43 \n  jmp irq_common_stub
-isr44: push 0  \n  push 44 \n  jmp irq_common_stub
-isr45: push 0  \n  push 45 \n  jmp irq_common_stub
-isr46: push 0  \n  push 46 \n  jmp irq_common_stub
-isr47: push 0  \n  push 47 \n  jmp irq_common_stub
+isr0:
+    push 0
+    push 0
+    jmp isr_common_stub
+isr1:
+    push 0
+    push 1
+    jmp isr_common_stub
+isr2:
+    push 0
+    push 2
+    jmp isr_common_stub
+isr3:
+    push 0
+    push 3
+    jmp isr_common_stub
+isr4:
+    push 0
+    push 4
+    jmp isr_common_stub
+isr5:
+    push 0
+    push 5
+    jmp isr_common_stub
+isr6:
+    push 0
+    push 6
+    jmp isr_common_stub
+isr7:
+    push 0
+    push 7
+    jmp isr_common_stub
+isr8:
+    push 8
+    push 8
+    jmp isr_common_stub
+isr9:
+    push 0
+    push 9
+    jmp isr_common_stub
+isr10:
+    push 0
+    push 10
+    jmp isr_common_stub
+isr11:
+    push 0
+    push 11
+    jmp isr_common_stub
+isr12:
+    push 0
+    push 12
+    jmp isr_common_stub
+isr13:
+    push 0
+    push 13
+    jmp isr_common_stub
+isr14:
+    push 0
+    push 14
+    jmp isr_common_stub
+isr15:
+    push 0
+    push 15
+    jmp isr_common_stub
+isr16:
+    push 0
+    push 16
+    jmp isr_common_stub
+isr17:
+    push 0
+    push 17
+    jmp isr_common_stub
+isr18:
+    push 0
+    push 18
+    jmp isr_common_stub
+isr19:
+    push 0
+    push 19
+    jmp isr_common_stub
+isr20:
+    push 0
+    push 20
+    jmp isr_common_stub
+isr21:
+    push 0
+    push 21
+    jmp isr_common_stub
+isr22:
+    push 0
+    push 22
+    jmp isr_common_stub
+isr23:
+    push 0
+    push 23
+    jmp isr_common_stub
+isr24:
+    push 0
+    push 24
+    jmp isr_common_stub
+isr25:
+    push 0
+    push 25
+    jmp isr_common_stub
+isr26:
+    push 0
+    push 26
+    jmp isr_common_stub
+isr27:
+    push 0
+    push 27
+    jmp isr_common_stub
+isr28:
+    push 0
+    push 28
+    jmp isr_common_stub
+isr29:
+    push 0
+    push 29
+    jmp isr_common_stub
+isr30:
+    push 0
+    push 30
+    jmp isr_common_stub
+isr31:
+    push 0
+    push 31
+    jmp isr_common_stub
+isr32:
+    push 0
+    push 32
+    jmp irq_common_stub
+isr33:
+    push 0
+    push 33
+    jmp irq_common_stub
+isr34:
+    push 0
+    push 34
+    jmp irq_common_stub
+isr35:
+    push 0
+    push 35
+    jmp irq_common_stub
+isr36:
+    push 0
+    push 36
+    jmp irq_common_stub
+isr37:
+    push 0
+    push 37
+    jmp irq_common_stub
+isr38:
+    push 0
+    push 38
+    jmp irq_common_stub
+isr39:
+    push 0
+    push 39
+    jmp irq_common_stub
+isr40:
+    push 0
+    push 40
+    jmp irq_common_stub
+isr41:
+    push 0
+    push 41
+    jmp irq_common_stub
+isr42:
+    push 0
+    push 42
+    jmp irq_common_stub
+isr43:
+    push 0
+    push 43
+    jmp irq_common_stub
+isr44:
+    push 0
+    push 44
+    jmp irq_common_stub
+isr45:
+    push 0
+    push 45
+    jmp irq_common_stub
+isr46:
+    push 0
+    push 46
+    jmp irq_common_stub
+isr47:
+    push 0
+    push 47
+    jmp irq_common_stub
 
 isr_common_stub:
     pusha
